936e6ca0e4a98f2794f969a2443d2fc76cd92a4e
585901 <111574775+585901@users.noreply.github.com>
2022-10-03 14:17:59 +0200

    Update Matriser.java

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -5,8 +5,12 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int i = 0; i<matrise.length; i++) {
+			for (int j = 0; j<matrise[i].length; j++) {
+				System.out.print(matrise[i][j] + " ");
+				System.out.println(); 
+			}
+		}
 	}
 
 	// b)
